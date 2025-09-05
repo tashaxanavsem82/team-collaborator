5e40a50fc4d50cdca3799cd3cfc92b968df3b6b0
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-05 13:22:50 -0500

    Improved logging message for collaboration start event.

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 document.getElementById('start-collaborating').addEventListener('click', function() {
-    console.log('Collaboration session started!');
+    console.log('Collaboration session has been initiated.');
 });
